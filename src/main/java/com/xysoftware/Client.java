3737dd0335f82934a0b34bf6863c494ae460f8d2
xuyang <274071580@163.com>
2025-05-18 12:43:28 +0800

    first

---
+++ src/main/java/com/xysoftware/Client.java
@@ -0,0 +1,8 @@
+package com.xysoftware;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World2");
+    }
+}
