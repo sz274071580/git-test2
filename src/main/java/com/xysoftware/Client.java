3b6706542178bbcb1d07708fd927bd0ee42d1dca
xuyang <274071580@163.com>
2025-05-18 13:28:41 +0800

    second

--- src/main/java/com/xysoftware/Client.java
+++ src/main/java/com/xysoftware/Client.java
@@ -8,5 +8,6 @@ public class Client {
         System.out.println("Hello World4");
         System.out.println("Hello World5");
         System.out.println("Hello World6");
+        System.out.println("push 123");
     }
 }
